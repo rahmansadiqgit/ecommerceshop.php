4880951576bb3652a6348ad4ef89187bc8ae4d38
Sadiqur-Rahman <rahman2305341270@diu.edu.bd>
2025-11-13 21:46:02 +0600

    password hashing removed for appling transposition criptograpic algorithm

--- login.php
+++ login.php
@@ -10,11 +10,12 @@ if(isset($_POST['login'])){
     $result = $conn->query("SELECT * FROM users WHERE email='$email'");
     $user   = $result->fetch_assoc();
 
-    if($user && password_verify($password, $user['password'])){
+    if($user && $password === $user['password']){
         $_SESSION['user_id'] = $user['id'];
         header("Location: index.php");
         exit;
-    } else {
+    }
+     else {
         echo "Invalid credentials!";
     }
 }
