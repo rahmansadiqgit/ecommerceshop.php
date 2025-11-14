4bc2a449705627082ae9553dc43398121ddb4faa
Sadiqur-Rahman <rahman2305341270@diu.edu.bd>
2025-11-14 19:36:05 +0600

    Encription applied to register.php

--- register.php
+++ register.php
@@ -1,16 +1,47 @@
 <?php
+session_start();
 include 'db.php';
 
+// Simple Columnar Transposition Cipher
+function encryptTransposition($text, $key = 8) {
+    $text = str_replace(' ', '', $text); // remove spaces
+    $columns = $key;
+    $rows = ceil(strlen($text) / $columns);
+    $matrix = array_fill(0, $rows, array_fill(0, $columns, ''));
+    
+    // Fill matrix row-wise
+    $k = 0;
+    for ($r = 0; $r < $rows; $r++) {
+        for ($c = 0; $c < $columns; $c++) {
+            if ($k < strlen($text)) {
+                $matrix[$r][$c] = $text[$k++];
+            }
+        }
+    }
+
+    // Read column-wise to get encrypted text
+    $encrypted = '';
+    for ($c = 0; $c < $columns; $c++) {
+        for ($r = 0; $r < $rows; $r++) {
+            $encrypted .= $matrix[$r][$c];
+        }
+    }
+
+    return $encrypted;
+}
+
 if(isset($_POST['register'])){
-    // Escape strings for safety
     $name     = $conn->real_escape_string($_POST['name']);
     $email    = $conn->real_escape_string($_POST['email']);
     $password = $_POST['password'];
     $address  = $conn->real_escape_string($_POST['address']);
 
-    // Insert user without card_number
+    // Encrypt the password
+    $encryptedPassword = encryptTransposition($password);
+
+    // Insert user with encrypted password
     $conn->query("INSERT INTO users (name, email, password, address) 
-                  VALUES ('$name', '$email', '$password', '$address')");
+                  VALUES ('$name', '$email', '$encryptedPassword', '$address')");
 
     // Assign card_number as user_id
     $user_id = $conn->insert_id;
@@ -20,25 +51,3 @@ if(isset($_POST['register'])){
     exit;
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Register</title>
-</head>
-<body>
-<a href="login.php">
-    <button type="button">Log In</button>
-</a>
-<a href="index.php"><button type="button">Back to Shop</button></a> 
-
-<h2>Register</h2>
-<form method="POST">
-    Name: <input type="text" name="name" required><br>
-    Email: <input type="email" name="email" required><br>
-    Password: <input type="password" name="password" required><br>
-    Address: <textarea name="address"></textarea><br>
-    <button type="submit" name="register">Register</button>
-</form>
-</body>
-</html>
